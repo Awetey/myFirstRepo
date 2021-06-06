cedba77f5e68cb8e87967a6a8a43a45a62a69b9e
Awetey <aweteyasmara@gmail.com>
2021-06-06 09:42:53 -0500

    calcu2

--- calcu/calculatormod.js
+++ calcu/calculatormod.js
@@ -22,7 +22,7 @@ exports.displays=function(req,res,vals){
           res.write(String(result));
     res.write("</p>");
     res.write(
-        "<a href='https://selamwoldu.github.io/MyFirstRepo/Node/myFirstApp/calculator.html'>Another calculation</a>"
+        //"<a href='https://selamwoldu.github.io/MyFirstRepo/Node/myFirstApp/calculator.html'>Another calculation</a>"
       );
     res.write("</body>");
     res.write("</html>");
