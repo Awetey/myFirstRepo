163cdcac71455981799d3418d01f4b47bbcebbd4
Awetey <aweteyasmara@gmail.com>
2021-06-06 09:26:28 -0500

    sundayMorning

--- myFirstApp/demo_db_connection.js
+++ myFirstApp/demo_db_connection.js
@@ -1,13 +1,91 @@
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Naomi@2021",
+//   database:"AyitaDB"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Naomi@2021",
-  database:"AyitaDB"
+  database: "mydb"
+  
 });
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("CREATE DATABASE mydb", function (err, result) {
+//     if (err) throw err;
+//     console.log("Database created");
+//   });
+// });
 
+// create Table 
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
+
+// Alter Table 
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table altered");
+//   });
+// });
+
+// Insert Multiple 
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO customers (name, address) VALUES ?";
+//   var values = [
+//     ['John', 'Highway 71'],
+//     ['Peter', 'Lowstreet 4'],
+//     ['Amy', 'Apple st 652'],
+//     ['Hannah', 'Mountain 21'],
+//     ['Michael', 'Valley 345'],
+//     ['Sandy', 'Ocean blvd 2'],
+//     ['Betty', 'Green Grass 1'],
+//     ['Richard', 'Sky st 331'],
+//     ['Susan', 'One way 98'],
+//     ['Vicky', 'Yellow Garden 2'],
+//     ['Ben', 'Park Lane 38'],
+//     ['William', 'Central st 954'],
+//     ['Chuck', 'Main Road 989'],
+//     ['Viola', 'Sideway 1633']
+//   ];
+//   con.query(sql, [values], function (err, result) {
+//     if (err) throw err;
+//     console.log("Number of records inserted: " + result.affectedRows);
+//   });
+// });
+
+// Select from mysql
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+ // con.query("SELECT * FROM customers where name like 'V%'", function (err, result, fields) {
+    //con.query("SELECT name,address FROM customers ", function (err, result, fields) {
+      con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
